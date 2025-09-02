5f97ce16a1cff64c472249fba9e4f9d109992c7f
venkatsiriki <24m11mc167@adityauniversity.in>
2025-09-02 11:37:57 +0530

    Trigger Vercel redeploy - update console log

--- MatrixFrontEndAlphaVersion/src/views/auth/SignIn.jsx
+++ MatrixFrontEndAlphaVersion/src/views/auth/SignIn.jsx
@@ -713,7 +713,7 @@ export default function SignIn() {
   };
 
   const handleGoogleLogin = () => {
-    console.log('Google login clicked');
+    console.log('Google login clicked - redeploy test');
     window.location.href = `${API_URL}/api/auth/google`;
   };
 
